20eb9a8bd11a77ef7ebc3849a8ba303b5f6ae069
AD-iT448 <arihades.123@gmail.com>
2024-05-17 11:16:26 -0400

    initial project portfolio

---
+++ dist/js/script.js
@@ -0,0 +1,88 @@
+//Navbar fixed
+
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
+
+    if(window.pageYOffset > fixedNav){
+        header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
+    }else{
+        header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
+    }
+}
+
+
+// Hamburger
+
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu'); 
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
+
+
+window.addEventListener('click', function(e){
+    if(e.target != hamburger && e.target != navMenu ){
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+        
+    }
+});
+
+// dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function(){
+if(darkToggle.checked){
+    html.classList.add('dark');
+}else{
+    html.classList.remove('dark');
+}
+});
+
+
+const contactForm = document.getElementById("contact-form");
+
+contactForm = addEventListener("submit", function(e){
+e.preventDefault();
+
+const url = e.target.action;
+
+const formData = new FormData(contactForm);
+
+fetch(url,{
+    method: "POST",
+    body: formData,
+    mode:"no-cors",
+}).then(()=>{
+    // url tank you
+    window.location.href= "thankyou.html";
+}).catch((e)=> alert('Error Occured'));
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
